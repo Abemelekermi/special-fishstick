1240c15b866fedfd64261691c717d30473eefae2
Abemelekermi <103841321+Abemelekermi@users.noreply.github.com>
2024-11-22 19:18:16 +0300

    Commit 2661 of 5000

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-import os
+import subprocess
+import concurrent.futures
 
 commitLen = int(input("How many times do you want to commit? \n"))
 autoPush = input("Auto git push? (y/n) \n")
 
-for i in range(commitLen):
-	os.system(f'git commit --allow-empty -m "Commit {i} of {commitLen}"')	
+# Function to perform a single commit
+def perform_commit(i, commitLen):
+    command = ['git', 'commit', '--allow-empty', '-m', f'Commit {i} of {commitLen}']
+    subprocess.run(command, check=True)
 
-print("Commited " + str(commitLen) + " times")
+# Use ThreadPoolExecutor for parallel execution
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = [executor.submit(perform_commit, i, commitLen) for i in range(commitLen)]
+    concurrent.futures.wait(futures)
+
+print("Committed " + str(commitLen) + " times")
 
 if autoPush == "y":
-	os.system('git push')
+    subprocess.run(['git', 'push'], check=True)
