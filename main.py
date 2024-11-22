a6950182be9c80e2584a8c11ddfc88e178484f3d
Abemelekermi <103841321+Abemelekermi@users.noreply.github.com>
2024-11-22 19:08:16 +0300

    j

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 import os
 
-ip = int(input("How many times do you want to commit? \n"))
-autoPush = input("Auto git push when commited? (y/n) \n")
+commitLen = int(input("How many times do you want to commit? \n"))
+autoPush = input("Auto git push? (y/n) \n")
 
-for i in range(ip):
-	# os.system('git commit --allow-empty -m "New Commit at: $(date)"')
-	os.system(f'git commit --allow-empty -m "Commit {i} of {ip}"')	
+for i in range(commitLen):
+	os.system(f'git commit --allow-empty -m "Commit {i} of {commitLen}"')	
 
-print("Commited " + str(ip) + " times")
+print("Commited " + str(commitLen) + " times")
 
 if autoPush == "y":
 	os.system('git push')
-	
-# git commit --allow-empty -m "New Commit at: $(date)"
-
-#  MADE BY:_            _   _____                        _ 
-#  \ \    / (_)        (_) |  __ \                      (_)
-#   \ \  / / _ _ __ ___ _  | |  | | __ _ ___  __ _ _ __  _ 
-#    \ \/ / | | '__/ _ \ | | |  | |/ _` / __|/ _` | '_ \| |
-#     \  /  | | | |  __/ | | |__| | (_| \__ \ (_| | | | | |
-#      \/   |_|_|  \___| | |_____/ \__,_|___/\__,_|_| |_|_|
-#                     _/ |                                 
-#                    |__/                                  
